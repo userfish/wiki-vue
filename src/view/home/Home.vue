7b96531fccd86d5181f1f8a964da25d58893f96d
Profile picture <49886808+userfish@users.noreply.github.com>
2022-03-03 20:26:10 +0800

    初始化2

---
+++ src/view/home/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+        app
+        :src="require('../../assets/right-back.png')"
+    >
+      <RightHome/>
+      <!-- -->
+    </v-navigation-drawer>
+
+
+    <!-- 根据应用组件来调整你的内容 -->
+
+    <v-main>
+      <v-img
+          :src="require('../../assets/backgroup.jpg')"
+      />
+    </v-main>
+
+  </v-app>
+</template>
+
+<script>
+import RightHome from "@/view/home/RightHome";
+export default {
+  name: "Home",
+  components: {RightHome}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/view/home/RightHome.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-btn
+      value="any"
+  >测试</v-btn>
+</template>
+
+<script>
+export default {
+  name: "RightHome"
+}
+</script>
+
+<style scoped>
+
+</style>
