c5a5bbcad82303b9565a3f0ce3361a42b4614f8f
Profile picture <49886808+userfish@users.noreply.github.com>
2022-04-27 22:40:57 +0800

    连接后端

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,22 @@ module.exports = defineConfig({
   lintOnSave: false,
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  // devServer: {
+  //   open: true, //是否自动弹出浏览器页面
+  //   host: "localhost",
+  //   port: '8081',
+  //   https: false,   //是否使用https协议
+  //   proxy: {
+  //     '/back-home': {
+  //       target: 'http://localhost:18080/back-home', //API服务器的地址
+  //       changeOrigin: true,
+  //       pathRewrite: {
+  //         '^/back-home': ''
+  //       }
+  //     }
+  //   }
+  // }
+
+
 })
